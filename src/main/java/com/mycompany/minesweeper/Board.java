9094f4775b6628fce6a3257a18bed9ec677ba202
victor <victor@ti2-21>
2024-03-14 20:47:28 +0100

    Small fixes 2

--- src/main/java/com/mycompany/minesweeper/Board.java
+++ src/main/java/com/mycompany/minesweeper/Board.java
@@ -86,6 +86,7 @@ public class Board extends javax.swing.JPanel {
     }
     
     private void processWin() {
+        gameOver = true;
         timerInterface.stop();
         JOptionPane.showMessageDialog(this, "You win");
     }
